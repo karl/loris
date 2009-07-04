8c4fff5336a8053bc23a0efc3adace5dbbc5cf38
Karl O'Keeffe <github@monket.net>
2009-07-04 15:19:37 +0100

    Added icons to Growl notifications.

--- lib/growl_output_decorator.rb
+++ lib/growl_output_decorator.rb
@@ -5,14 +5,21 @@ class GrowlOutputDecorator
     @growler = growler
   end
   
-  def add_result(result)    
-      @growler.notify {
-        self.title = result[:title]
-        self.message = result[:summary]
-        self.icon = :ruby
-      }
+  def add_result(result)
+    icon = get_icon(result[:state])
+    puts icon
+
+    @growler.notify {
+      self.title = result[:title]
+      self.message = result[:summary]
+      self.image = icon
+    }
     
-      @output.add_result result
+    @output.add_result result
+  end
+  
+  def get_icon(state)
+    return File.join File.expand_path(File.dirname(__FILE__)), 'icons', "#{state.to_s}.png"
   end
   
 end
