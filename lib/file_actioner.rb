0185deff35727e95e164a95d2c3315383a29058b
Karl O'Keeffe <github@monket.net>
2010-07-07 08:43:09 +0100

    * Added error handling around the file_actioner. This should fix issues where loris was crash if a file was removed/changed while it was attempting to get the modified time.

--- lib/file_actioner.rb
+++ lib/file_actioner.rb
@@ -8,17 +8,21 @@ class FileActioner
   end
   
   def run
-    files = @file_finder.find
+    begin
+      files = @file_finder.find
     
-    # Refactor this to the file_finder class
-    changes = (files[:all] - @prev_all_files) | (@prev_all_files - files[:all])
-    files[:filtered] = files[:filtered] | changes
+      # Refactor this to the file_finder class
+      changes = (files[:all] - @prev_all_files) | (@prev_all_files - files[:all])
+      files[:filtered] = files[:filtered] | changes
     
-    if (files[:filtered] != [])
-      @task_manager.run(files)
+      if (files[:filtered] != [])
+        @task_manager.run(files)
+      end
+    
+      @prev_all_files = files[:all]
+    rescue Exception => e
+      @task_manager.output_exception(e);
     end
-    
-    @prev_all_files = files[:all]
   end
   
 end
