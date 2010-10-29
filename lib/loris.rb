9e5eb48e1c3266e9ff5da00e41cc870a9318996e
Karl O'Keeffe <github@monket.net>
2010-10-29 17:33:09 +0100

    Don't run jasmine node coverage as it is not yet fully baked.

--- lib/loris.rb
+++ lib/loris.rb
@@ -29,8 +29,8 @@ require 'filters/starts_with_filter'
 
 require 'outputs/output_collection'
 require 'outputs/shell_output'
-require 'outputs/windows_console_clearing_output' 
-require 'outputs/unix_console_clearing_output' 
+require 'outputs/windows_console_clearing_output'
+require 'outputs/unix_console_clearing_output'
 require 'outputs/growl_output'
 
 require 'tasks/list_task'
@@ -68,30 +68,30 @@ end
 module Loris
   BINARY = File.expand_path(File.dirname(__FILE__) + '/../bin/loris')
   LIBDIR = File.expand_path(File.dirname(__FILE__) + '/../lib')
-  RUBY_BINARY = File.join(Config::CONFIG['bindir'], Config::CONFIG['ruby_install_name'])  
-  
+  RUBY_BINARY = File.join(Config::CONFIG['bindir'], Config::CONFIG['ruby_install_name'])
+
   module CLI
-    
+
     class Main
-    
+
       class DummyActioner
-        
+
         def initialize(actioner, stream)
           @actioner = actioner
           @stream = stream
         end
-        
+
         def run
             @actioner.run
             @stream.puts '[Poll complete]'
             @stream.flush
         end
-        
+
       end
-    
+
       class << self
         def execute(args)
-          
+
           # Get config variables
           debug = args.length > 0
           is_windows = RUBY_PLATFORM =~ /mswin32/
@@ -111,31 +111,31 @@ module Loris
           oc = OutputCollection.new
           oc.add(ShellOutput.new($stdout))
           oc.add(cco) unless debug
-          oc.add(GrowlOutput.new(Growl)) unless debug       
-          
+          oc.add(GrowlOutput.new(Growl)) unless debug
+
           tm = TaskManager.new(oc)
           tm.add(ListTask.new) if debug
           tm.add(coffeescript_task(dir))
           tm.add(javascript_lint_task(dir))
           tm.add(google_lint_task(dir))
           tm.add(jasmine_node_task(dir))
-          tm.add(jasmine_node_coverage_task(dir))
+          # tm.add(jasmine_node_coverage_task(dir))
           tm.add(CommandLineTask.new(JSpecRunner.new(dir, ExtensionFilter.new(File, 'js')), JSpecParser.new)) unless is_windows
           tm.add(jsTestDriverTask(dir))
           tm.add(CommandLineTask.new(RSpecRunner.new(dir, ExtensionFilter.new(File, 'rb'), EndsWithFilter.new('_spec.rb')), RSpecParser.new))
           tm.add(closure_compiler_task(dir))
 
-          a = FileActioner.new(ff, tm)    
-          
+          a = FileActioner.new(ff, tm)
+
           da = DummyActioner.new(a, $stdout)
-                
+
           p = Poller.new(w, c, debug ? da : a)
-          
+
           # Start!
           p.start
 
         end
-        
+
         def coffeescript_task(dir)
           return CommandLineTask.new(
             CoffeeScriptRunner.new(
@@ -147,11 +147,11 @@ module Loris
             CoffeeScriptParser.new(dir)
           )
         end
-        
+
         def jasmine_node_task(dir)
           node = '/usr/local/bin/node'
           spec_dir = File.join(LIBDIR, 'jasmine-node')
-          
+
           return CommandLineTask.new(
             JasmineNodeRunner.new(
               node,
@@ -163,7 +163,7 @@ module Loris
             JasmineNodeParser.new(dir)
           )
         end
-        
+
         def jasmine_node_coverage_task(dir)
           node = '/usr/local/bin/node'
           spec_dir = File.join(LIBDIR, 'jasmine-node')
@@ -183,112 +183,112 @@ module Loris
             JasmineNodeParser.new(dir)
           )
         end
-        
+
         # Refactor into factory
         def javascript_lint_task(dir)
           is_windows = RUBY_PLATFORM =~ /mswin32/
           binary = File.join(LIBDIR, 'javascript-lint' , is_windows ? 'jsl.exe' : 'jsl')
-          
+
           return CommandLineTask.new(
             JavascriptLintRunner.new(
               binary,
-              dir, 
+              dir,
               ExtensionFilter.new(File, 'js')
-            ), 
+            ),
             JavascriptLintParser.new(dir)
-          )          
+          )
         end
-        
+
         def google_lint_task(dir)
           binary = File.join(LIBDIR, 'google-lint' , 'gjslint')
           fix_style_binary = File.join(LIBDIR, 'google-lint' , 'fixjsstyle')
-          
+
           return CommandLineTask.new(
             GoogleLintRunner.new(
               binary,
               fix_style_binary,
-              dir, 
+              dir,
               ExtensionFilter.new(File, 'js'),
               GoogleLintConfig.new(
-                dir, 
-                YAML, 
+                dir,
+                YAML,
                 URI
               )
-            ), 
+            ),
             GoogleLintParser.new(dir)
-          )          
+          )
         end
-        
+
         def closure_compiler_task(dir)
           jar = File.join(LIBDIR, 'closure-compiler' , 'closure-compiler.jar')
-          
+
           return CommandLineTask.new(
             ClosureCompilerRunner.new(
               jar,
-              dir, 
+              dir,
               ExtensionFilter.new(File, 'js'),
               ClosureCompilerConfig.new(
-                dir, 
-                YAML, 
+                dir,
+                YAML,
                 URI
               )
-            ), 
+            ),
             ClosureCompilerParser.new(dir)
-          )          
+          )
         end
-        
-        
+
+
         # Will need to be refactored into a factory
         def jsTestDriverTask(dir)
           require 'tasks/js_test_driver/js_test_driver_runner'
           require 'tasks/js_test_driver/js_test_driver_parser'
           require 'tasks/js_test_driver/js_test_driver_config'
-          require 'tasks/js_test_driver/js_test_driver_server'          
-          
+          require 'tasks/js_test_driver/js_test_driver_server'
+
           jar = File.join(LIBDIR, 'js-test-driver/JsTestDriver-1.2.jar')
           is_windows = RUBY_PLATFORM =~ /mswin32/
-          
-          if is_windows 
+
+          if is_windows
             require 'browser_finder'
             browser = BrowserFinder.new.getDefault
           else
             browser = 'open "%1"'
           end
-          
+
           sleep_time = 3
-          
+
           if is_windows
             require 'windows_process'
           else
             require 'unix_process'
           end
-          
+
           return CommandLineTask.new(
             JsTestDriverRunner.new(
-              dir, 
-              jar, 
+              dir,
+              jar,
               ExtensionFilter.new(File, 'js'),
               JsTestDriverServer.new(
                 JsTestDriverConfig.new(
-                  dir, 
-                  YAML, 
+                  dir,
+                  YAML,
                   URI
-                ), 
-                Pinger.new, 
+                ),
+                Pinger.new,
                 is_windows ? WindowsProcess.new : UnixProcess.new,
-                jar, 
+                jar,
                 browser,
                 sleep_time
               )
-            ), 
+            ),
             JsTestDriverParser.new
-          )          
+          )
         end
-        
-      end    
+
+      end
 
     end
-    
+
   end
-  
+
 end
